cc0253eceb623d1e8f87e07da01e1626e1b98f86
switch <q547550831@outlook.com>
2016-07-01 15:31:25 +0800

    Fixed compiler error: Implemented method StubSensor.popNextPressurePsiValue().

--- src/main/java/kata/tpms/StubSensor.java
+++ src/main/java/kata/tpms/StubSensor.java
@@ -4,7 +4,14 @@ package kata.tpms;
  * Created by switch on 16/7/1.
  */
 public class StubSensor implements Transducer{
-    public void arrangeNextPressurePsiValue(double nextPressurePsiValue) {
+    private double nextPressurePsiValue;
 
+    public void arrangeNextPressurePsiValue(double nextPressurePsiValue) {
+        this.nextPressurePsiValue = nextPressurePsiValue;
+    }
+
+    @Override
+    public double popNextPressurePsiValue() {
+        return this.nextPressurePsiValue;
     }
 }
